8d2ef78c49dc5d98f413b360073b57b9b307ef62
Sylvan Bowdler <sylvanbowdler18@gmail.com>
2022-05-09 00:58:35 +0100

    Add environment setup for Prod/local + added Docker caching

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use sqlx::PgPool;
 use std::net::TcpListener;
 use secrecy::ExposeSecret;
+use sqlx::postgres::PgPoolOptions;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
@@ -11,12 +12,15 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
-    let connection_pool = PgPool::connect(
-        &configuration.database.connection_string().expose_secret()
-        ).await
-        .expect("Failed to connect to Postgres.");
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(
+            &configuration.database.connection_string().expose_secret()
+        )
+        .expect("Failed to create Postgres connection pool");
 
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+
+    let address = format!("127.0.0.1:{}", configuration.application.port);
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
     run(listener, connection_pool)?.await
 }
